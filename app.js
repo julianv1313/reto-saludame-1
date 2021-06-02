4a980124bb9648c06a516eb169593eb336fd2cc4
julianv1313 <jv.julian@hotmail.com>
2021-06-01 20:40:58 -0500

    Capitalización

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Hola  ${req.query.nombre?req.query.nombre : "desconocido"}!</h1>`);
+    res.send(`<h1>Hola  ${req.query.nombre?req.query.nombre.charAt().toUpperCase() + req.query.nombre.slice(1) : "desconocido"}!</h1>`);
   });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
